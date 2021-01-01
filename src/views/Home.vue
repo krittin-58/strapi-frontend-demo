04a4ddda78f8c2451d09643d3bcb83c0e821e726
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2021-01-01 22:56:37 +0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <ul v-else>
+      <li v-for="restaurant in restaurants" :key="restaurant.id">
+        {{ restaurant.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      restaurants: [],
+      error: null,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  },
+
+  async mounted() {
+    try {
+      const response = await fetch('http://localhost:1337/restaurants', {
+        method: 'GET',
+        headers: this.headers,
+      }).then(this.parseJSON);
+      this.restaurants = response;
+    } catch (error) {
+      this.error = error;
+    }
+  },
+
+  methods: {
+    parseJSON(resp) {
+      return (resp.json ? resp.json() : resp);
+    },
+
+    // checkStatus(resp) {
+    //   if (resp.status >= 200 && resp.status < 300) {
+    //     return resp;
+    //   }
+    //   return this.parseJSON(resp).then((resp) => {
+    //     throw resp;
+    //   });
+    // },
+
+    getRestaurants() {
+      fetch('http://localhost:1337/restaurants', {
+        method: 'GET',
+        headers: this.headers,
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
   },
 };
 </script>
